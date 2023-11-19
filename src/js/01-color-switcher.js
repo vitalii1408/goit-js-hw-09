82488046e9dee95b3973ce5f47b46b5bc37fb1f1
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-19 21:24:06 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.getElementById('startBtn');
+const stopButton = document.getElementById('stopBtn');
+let intervalId = null;
+
+startButton.addEventListener('click', startChangingColor);
+stopButton.addEventListener('click', stopChangingColor);
+
+function startChangingColor() {
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+}
+
+function stopChangingColor() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
