5ddd4614af1df467be729e2a3fe6162d4955f4dd
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-23 20:34:18 +0200

    10

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -12,23 +14,29 @@ function createPromise(position, delay) {
   });
 }
 
-document.getElementById('promiseForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+document
+  .getElementById('promiseForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
 
-  const formData = new FormData(event.target);
-  const firstDelay = parseInt(formData.get('delay'));
-  const delayStep = parseInt(formData.get('step'));
-  const amount = parseInt(formData.get('amount'));
+    const formData = new FormData(event.target);
+    const firstDelay = parseInt(formData.get('delay'));
+    const delayStep = parseInt(formData.get('step'));
+    const amount = parseInt(formData.get('amount'));
 
-  for (let i = 1; i <= amount; i++) {
-    const currentDelay = firstDelay + (i - 1) * delayStep;
+    for (let i = 1; i <= amount; i++) {
+      const currentDelay = firstDelay + (i - 1) * delayStep;
 
-    createPromise(i, currentDelay)
-      .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-  }
-});
+      createPromise(i, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.Success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.Failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  });
