557a3de620a2c7bd2bfef9975eb66e536ae200c2
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-24 18:20:09 +0000

    Deploying to gh-pages from @ vitalii1408/goit-js-hw-09@597abc3bf59665e4f82c3102268ade435add4ab4 🚀

---
+++ 01-color-switcher.3d8014b0.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t||(e.disabled=!0,d.disabled=!1,t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(t),t=null}));
+//# sourceMappingURL=01-color-switcher.3d8014b0.js.map
