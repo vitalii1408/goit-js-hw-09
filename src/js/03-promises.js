85b60c544467f557b81a8220266717b6c2617f95
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-24 20:42:57 +0200

    14

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,6 @@
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -11,26 +10,37 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
 document
   .getElementById('promiseForm')
   .addEventListener('submit', function (event) {
     event.preventDefault();
-
     const formData = new FormData(event.target);
     const firstDelay = parseInt(formData.get('delay'));
     const delayStep = parseInt(formData.get('step'));
     const amount = parseInt(formData.get('amount'));
+    const submitButton = document.querySelector('[type="submit"]');
+    const delayInput = document.querySelector('[name="delay"]');
+    const stepInput = document.querySelector('[name="step"]');
+    const amountInput = document.querySelector('[name="amount"]');
+    submitButton.disabled = true;
+    (async function () {
+      for (let i = 1; i <= amount; i++) {
+        const currentDelay = firstDelay + (i - 1) * delayStep;
 
-    for (let i = 1; i <= amount; i++) {
-      const currentDelay = firstDelay + (i - 1) * delayStep;
-
-      createPromise(i, currentDelay)
-        .then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-    }
+        try {
+          const result = await createPromise(i, currentDelay);
+          console.log(
+            `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
+          );
+        } catch (error) {
+          console.log(
+            `❌ Rejected promise ${error.position} in ${error.delay}ms`
+          );
+        }
+      }
+      submitButton.disabled = false;
+      delayInput.value = '';
+      stepInput.value = '';
+      amountInput.value = '';
+    })();
   });
