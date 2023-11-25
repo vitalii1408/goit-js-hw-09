62ff0ef083b3ea123ae003a05a84c2c6ef437be2
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-25 19:14:47 +0200

    16

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,9 +10,10 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
 document
   .getElementById('promiseForm')
-  .addEventListener('submit', function (event) {
+  .addEventListener('submit', async function (event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const firstDelay = parseInt(formData.get('delay'));
@@ -23,24 +24,24 @@ document
     const stepInput = document.querySelector('[name="step"]');
     const amountInput = document.querySelector('[name="amount"]');
     submitButton.disabled = true;
-    (async function () {
-      for (let i = 1; i <= amount; i++) {
-        const currentDelay = firstDelay + (i - 1) * delayStep;
 
-        try {
-          const result = await createPromise(i, currentDelay);
-          console.log(
-            `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
-          );
-        } catch (error) {
-          console.log(
-            `❌ Rejected promise ${error.position} in ${error.delay}ms`
-          );
-        }
+    for (let i = 1; i <= amount; i++) {
+      const currentDelay = firstDelay + (i - 1) * delayStep;
+
+      try {
+        const result = await createPromise(i, currentDelay);
+        console.log(
+          `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
+        );
+      } catch (error) {
+        console.log(
+          `❌ Rejected promise ${error.position} in ${error.delay}ms`
+        );
       }
-      submitButton.disabled = false;
-      delayInput.value = '';
-      stepInput.value = '';
-      amountInput.value = '';
-    })();
+    }
+
+    submitButton.disabled = false;
+    delayInput.value = '';
+    stepInput.value = '';
+    amountInput.value = '';
   });
