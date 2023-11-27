078e3de2a08f1209ca680c81c7c131ce2b0b5c0e
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-27 22:22:52 +0200

    17

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,11 +2,9 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
+      shouldResolve
+        ? resolve({ position, delay })
+        : reject({ position, delay });
     }, delay);
   });
 }
@@ -24,24 +22,25 @@ document
     const stepInput = document.querySelector('[name="step"]');
     const amountInput = document.querySelector('[name="amount"]');
     submitButton.disabled = true;
-
-    for (let i = 1; i <= amount; i++) {
-      const currentDelay = firstDelay + (i - 1) * delayStep;
-
-      try {
-        const result = await createPromise(i, currentDelay);
-        console.log(
-          `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
-        );
-      } catch (error) {
-        console.log(
-          `❌ Rejected promise ${error.position} in ${error.delay}ms`
-        );
-      }
+    const promises = [];
+    for (let i = 0; i < amount; i++) {
+      const currentDelay = firstDelay + i * delayStep;
+      const promise = createPromise(i, currentDelay);
+      promise
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      promises.push(promise);
     }
-
-    submitButton.disabled = false;
-    delayInput.value = '';
-    stepInput.value = '';
-    amountInput.value = '';
+    try {
+      await Promise.allSettled(promises);
+    } finally {
+      submitButton.disabled = false;
+      delayInput.value = '';
+      stepInput.value = '';
+      amountInput.value = '';
+    }
   });
